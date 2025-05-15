4aac9570df2c1dd5ef5d7470e290467b1b1e05fe
RodrigoB <robi.boehme@gmail.com>
2025-05-14 21:52:18 -0300

    Add files via upload

---
+++ 2025/05-14/arrays/Arrays.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arrays;
+
+/**
+ *
+ * @author RodrigoBoehme
+ */
+public class Arrays {
+
+    public static void main(String[] args) {
+        int[] numeros=new int[5];
+        System.out.println(numeros);
+    }
+}
